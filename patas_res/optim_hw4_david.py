3b51f1b81239b1518fb5d12a7153793c8b2182e1
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-04-28 17:48:21 -0700

    initial commit

---
+++ patas_res/optim_hw4_david.py
@@ -0,0 +1,26 @@
+from typing import Iterable
+
+import numpy as np
+from edugrad.optim import Optimizer, SGD
+from edugrad.tensor import Tensor
+
+class Adagrad(Optimizer):
+
+    def __init__(self, params: Iterable[Tensor], lr: float = 1e-2):
+        super(Adagrad, self).__init__(params)
+        self.lr = lr
+        self._eps = 1e-7
+        # initialize squared gradient history for each param
+        for param in self.params:
+            param._grad_hist = np.zeros(param.value.shape)
+
+    def step(self):
+        for param in self.params:
+            # increment squared gradient history; param.grad contains the gradient
+            gradient_hist = param._grad_hist
+            gradient_hist += param.grad ** 2
+            # get adjusted learning rate
+            adjusted_learning_rate = self.lr / np.sqrt(gradient_hist + self._eps)
+            # update parameters
+            param.value -= adjusted_learning_rate * param.grad
+        self._cur_step += 1
